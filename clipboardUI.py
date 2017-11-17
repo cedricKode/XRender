4d53c80e699befd979092e76264f11c2a095ca54
Mox812 <cedric.kode@gmail.com>
2017-11-17 17:22:14 +0100

    Create clipboardUI.py

---
+++ clipboardUI.py
@@ -0,0 +1,7 @@
+import PyQt4.QtGui as QtGui
+import PyQt4.QtCore as QtCore
+import sys
+
+class ClipboardTab(QtGui.QTabWidget):
+    def __init__(self):
+        super(ClipboardTab,self, ).__init__()                    
