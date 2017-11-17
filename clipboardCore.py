10cf3bdccdc194c1d89205a1575601d5d167c88c
Mox812 <cedric.kode@gmail.com>
2017-11-17 17:23:01 +0100

    Create clipboardCore.py

---
+++ clipboardCore.py
@@ -0,0 +1,26 @@
+import PyQt4.QtGui as QtGui
+import PyQt4.QtCore as QtCore
+import sys
+
+from clipboardUi import ClipboardTab
+
+
+
+
+class ClipboardCore(ClipboardTab):
+    def __init__(self):
+        super(ClipboardCore,self).__init__()
+        
+        self.setWindowTitle("XRender")
+        self.setWindowFlags(QtCore.Qt.WindowStaysOnTopHint)
+        self.resize(1200,600)
+        self.setMinimumSize(600,300)
+        
+        #Widgets
+
+
+        
+app = QtGui.QApplication(sys.argv)
+tool = ClipboardCore()
+tool.show()
+app.exec_()
