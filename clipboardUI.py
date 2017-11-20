d66ab0cc7599c9b212a99013886b810e3f9d7271
Mox812 <cedric.kode@gmail.com>
2017-11-20 16:43:32 +0100

    Update clipboardUI.py

--- clipboardUI.py
+++ clipboardUI.py
@@ -13,30 +13,120 @@ class ClipboardTab(QtGui.QTabWidget):
         self.resize(1200,600)
         self.setMinimumSize(600,300)
         
+        #Widgets      
+        #---SCENEGRAPH
+        #Scene List widget
+        scenaListlabel = QtGui.QLabel("Scenes")
+        self.sceneListWidget = QtGui.QListView()
+        self.sceneListWidget.setDragEnabled(True)
+        #Search widget 
+        searchSceneLabel = QtGui.QLabel("Search")
+        self.searchSceneWidget = QtGui.QLineEdit()
+        #Custom Slider
+        self.customSliderleft = customSlider()
+        self.customSliderRight = customSlider()
+        #Stack widget
+        stackLabel = QtGui.QLabel("Stack")
+        self.stackWidget = QtGui.QListWidget()
+        self.stackWidget.setAcceptDrops(True)
+        #Text No Edit
+        self.textNotetextEdit  = QtGui.QPlainTextEdit()
+        #Add Button widget
+        self.Addbutton = QtGui.QPushButton("Add")
+        #Remove button widget
+        self.Rmvbutton = QtGui.QPushButton("Remove")
+        
+        #Main Widget
+        self.sceneGraphMainWidget = QtGui.QWidget()
         
         
-        #Widgets
-        renderSettingsLabel = QtGui.QLabel("Render Settings")
-        self.renderSettingsLabel_widget = QtGui.QTreeView()
-        self.renderSettingsLabel_widget.setDragEnabled(True)
         
-        search_label = QtGui.QLabel("Search")
-        self.search_label_edit = QtGui.QLineEdit()
-        renderView_label = QtGui.QLabel("Renderview")
-        self.renderView_label_Frame = QtGui.QFrame()
-        self.renderView_label_Frame.setAcceptDrops(True)
-        self.text_note_text_edit = QtGui.QPlainTextEdit()
-        self.render_button = QtGui.QPushButton("Render")
+        #---Layout ---     
+        #Main Layout
+        mainLayout = QtGui.QHBoxLayout()
         
-        self.main_widget = QtGui.QWidget()
-            
-        #Layout
-        hlayout = QtGui.QVBoxLayout()
-        hlayout.addWidget(renderSettingsLabel)
-        hlayout.addWidget(self.renderSettingsLabel_widget)
+        # Main  Left Layout
+        mainLayoutLeft = QtGui.QVBoxLayout()
+        mainLayoutLeft.addWidget(scenaListlabel)
+        mainLayoutLeft.addWidget(self.sceneListWidget)
+        searchLayout = QtGui.QHBoxLayout()
+        searchLayout.addWidget(searchSceneLabel)
+        searchLayout.addWidget(self.searchSceneWidget)
+        mainLayoutLeft.addLayout(searchLayout)
+        mainLayoutLeft.addWidget(self.customSliderleft)
         
-        #received Layout 
-        received_Layout = QtGui.QHBoxLayout()
-        received_Layout.addLayout(hlayout)
+        # Main Right Layout
+        mainLayoutRight = QtGui.QVBoxLayout()
+        mainLayoutRight.addWidget(stackLabel)
+        mainLayoutRight.addWidget(self.stackWidget)
+        mainLayoutRight.addWidget(self.textNotetextEdit)
+        mainLayoutRight.addWidget(self.customSliderRight)
+        btnLayout = QtGui.QHBoxLayout()
+        btnLayout.addWidget(self.Addbutton) 
+        btnLayout.addWidget(self.Rmvbutton)
+        mainLayoutRight.addLayout(btnLayout)
+             
+        # Add Layout to main Layout 
+        mainLayout.addLayout(mainLayoutLeft)
+        mainLayout.addLayout(mainLayoutRight)
         
+        # set main Layout to main Widget
+        self.sceneGraphMainWidget.setLayout(mainLayout)
+        
+        # Add table to Clipboard
+        self.addTab(self.sceneGraphMainWidget, "SceneGraph")
+
+
+class customSlider(QtGui.QSlider):
+
+    def __init__(self, mini=0, maxi=100, color=None):
+        super(customSlider, self).__init__()
+        self.mini = mini
+        self.maxi = maxi
+        self.setStyleSheet(self.stylesheet())
+        self.setOrientation(QtCore.Qt.Horizontal)
+        self.setTickPosition(QtGui.QSlider.TicksAbove)
+        self.setMouseTracking(True)
+        self.setMouseTracking(True)
+      
+      
+      #Layout
+    def sliderLayout(self):
+        self.slider = customSlider()        
+        sliderLayout = QtGui.QVBoxLayout()
+        sliderLayout.addWidget(self.slider)
+        self.setLayout(sliderLayout)
+
+    def stylesheet(self):
+        return """ 
+        QSlider::groove:horizontal {
+        height: 4px;
+        border: 1px solid black;
+        border-radius: 3px;
+        background: orange;
+        }
+        
+        QSlider::handle:horizontal {
+        border: 1px solid black;
+        height: 4px;
+        width: 3px;
+        border-radius: 1px ;
+        background: darkGray;
+        margin-top: -4px;
+        margin-bottom: -4px;
+        }
+
+        QSlider::sub-page:horizontal {
+        background: qlineargradient(x1: 0, y1: 0,    x2: 0, y2: 1,stop: 0 white, stop: 1 orange);
+        background: qlineargradient(x1: 0, y1: 0.2, x2: 1, y2: 1, stop: 0 white, stop: 1 orange);
+        height: 5px;
+        border: 1px solid black;
+        border-radius: 3px;
+        }
+        
+
+        
+        """
+
+
              
