5bd26827d57ac4f33188aef98f364268d0dd64ab
Mox812 <cedric.kode@gmail.com>
2017-11-21 15:26:50 +0100

    Update clipbordUI2.py

--- clipbordUI2.py
+++ clipbordUI2.py
@@ -3,85 +3,113 @@ import PyQt4.QtCore as QtCore
 import sys
 
 
-class ClipboardTab(QtGui.QTabWidget):
+
+class sceneGraph(QtGui.QTabWidget):
+    
+    
+    # Signals
+    btnClick = QtCore.pyqtSignal()
+
+    
     def __init__(self):
-        super(ClipboardTab,self, ).__init__()
+        super(sceneGraph,self, ).__init__()
+        #self.signals = emitSignal()
         
         
         self.setWindowTitle("XRender")
         self.setWindowFlags(QtCore.Qt.WindowStaysOnTopHint)
         self.resize(1200,600)
         self.setMinimumSize(600,300)
+        # Slots connector
+        s
+        elf.btnClick.connect(self.click)
+        #self.quitBtn.connect(click)
         
-        #Widgets      
+        #Widgets
         #---SCENEGRAPH
-        #Scene List widget
+        #Scene List widget---
         scenaListlabel = QtGui.QLabel("Scenes")
         self.sceneListWidget = QtGui.QListView()
         self.sceneListWidget.setDragEnabled(True)
+        
+        #LCD TEST SIGNAL
+        self.lcd = QtGui.QLCDNumber(self)
+        
         #Search widget 
         searchSceneLabel = QtGui.QLabel("Search")
         self.searchSceneWidget = QtGui.QLineEdit()
+        
         #Custom Slider
         self.customSliderleft = customSlider()
-        self.customSliderRight = customSlider()
+    
+        
         #Stack widget
         stackLabel = QtGui.QLabel("Stack")
         self.stackWidget = QtGui.QListWidget()
         self.stackWidget.setAcceptDrops(True)
+        
         #Text No Edit
         self.textNotetextEdit  = QtGui.QPlainTextEdit()
-        #Add Button widget
-        self.Addbutton = QtGui.QPushButton("Add")
-        #Remove button widget
-        self.Rmvbutton = QtGui.QPushButton("Remove")
         
+        #Add Button widget
+        self.Addbutton = QtGui.QPushButton("Open")
+        
+        #Quit button widget
+        self.quitbutton = QtGui.QPushButton("Quit")
+        self.quitbutton.clicked.connect(self.buttonClicked)
+
         #Main Widget
         self.sceneGraphMainWidget = QtGui.QWidget()
         
+        #QpushButton
+        btnLayout = QtGui.QHBoxLayout()
+        btnLayout.addWidget(self.Addbutton) 
+        #btnLayout.addWidget(self.quitbutton)
+                    
         
-        
-        #---Layout ---     
+        #---Layout init---     
         #Main Layout
         mainLayout = QtGui.QHBoxLayout()
         vboxLeft = QtGui.QVBoxLayout()
         vboxRight = QtGui.QVBoxLayout()
+           
         
         # Left Layout        
         vboxLeft.addWidget(scenaListlabel)
         vboxLeft.addWidget(self.sceneListWidget)
+        vboxLeft.addWidget(self.lcd)
+        vboxLeft.addWidget(self.customSliderleft)
         searchLayout = QtGui.QHBoxLayout()
         searchLayout.addWidget(searchSceneLabel)
         searchLayout.addWidget(self.searchSceneWidget)
+        vboxLeft.addLayout(btnLayout)
         vboxLeft.addLayout(searchLayout)
-        
-        
+                
         # Right Layout
         vboxRight.addWidget(stackLabel)
         vboxRight.addWidget(self.stackWidget)
         vboxRight.addWidget(self.textNotetextEdit)
-
-        #btnLayout = QtGui.QHBoxLayout()
-        #btnLayout.addWidget(self.Addbutton) 
-        #btnLayout.addWidget(self.Rmvbutton)
-        #mainLayoutRight.addLayout(btnLayout)
-             
-        # Add Layout to main Layout 
-        
+            
+        # Add Layouts to main Layout        
         mainLayout.addLayout(vboxLeft)
-        mainLayout.addLayout(vboxRight)
+        #mainLayout.addLayout(vboxRight)
         #mainLayout.addLayout(mainLayoutRight)
-        
-        
-        
+       
         
         # set main Layout to main Widget
         self.sceneGraphMainWidget.setLayout(mainLayout)
         
-        # Add table to Clipboard
+        # Add table to Main Window
         self.addTab(self.sceneGraphMainWidget, "SceneGraph")
         
-
+    def buttonClicked(self, checked=False):
+        self.btnClick.emit()
+        
+    def click(self):
+    
+        print "Simple custom signal:",
+        print "No arguments in this type of signal."
+       
 class customSlider(QtGui.QSlider):
 
     def __init__(self, mini=0, maxi=100, color=None):
@@ -93,15 +121,15 @@ class customSlider(QtGui.QSlider):
         self.setTickPosition(QtGui.QSlider.TicksAbove)
         self.setMouseTracking(True)
         self.setMouseTracking(True)
+        
       
-      
-      #Layout
+    #Layout
     def sliderLayout(self):
         self.slider = customSlider()        
         sliderLayout = QtGui.QVBoxLayout()
         sliderLayout.addWidget(self.slider)
         self.setLayout(sliderLayout)
-
+        
     def stylesheet(self):
         return """ 
         QSlider::groove:horizontal {
